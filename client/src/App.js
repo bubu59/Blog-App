76ddb067463da144a657344fd927057aee5d2b39
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-12 21:49:10 +0800

    setting up new react-router-dom on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is home!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Login = () => {
+const Navbar = () => {
     return (
         <div>
-            Login
+            Navbar
         </div>
     )
 }
 
-export default Login
+export default Navbar
