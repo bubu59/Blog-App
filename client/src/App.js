747fcfdccc64b1ea0a6510b0bf0372bbf4a724e5
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-13 22:29:38 +0800

    new way of representing pages with static components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,46 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route
+  Route,
+  Outlet
 } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Write from "./pages/Write";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is home!</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/post/:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+
+    ]
   },
   {
     path: "/register",
@@ -39,10 +67,13 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
+
 
 export default App;
