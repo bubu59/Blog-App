9226c9ca83932c0a65da0d951f6d8da9e2c73b06
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-15 16:56:34 +0800

    footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Logo from "../img/logo.PNG"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <img src={Logo} alt="" />
+            <span>Made with love and <b>React.js</b></span>
+        </footer>
     )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Logo from "../img/logo.PNG"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="container">
-                <div className="logo">logo</div>
-                <div className="links">links</div>
+                <div className="logo">
+                    <img src={Logo} alt='' />
+                </div>
+                <div className="links">
+                    <Link className='link' to="/?cat=art">
+                        <h6>ART</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=science">
+                        <h6>SCIENCE</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=technology">
+                        <h6>TECHNOLOGY</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=cinema">
+                        <h6>CINEMA</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=design">
+                        <h6>DESIGN</h6>
+                    </Link>
+                    <Link className='link' to="/?cat=food">
+                        <h6>FOOD</h6>
+                    </Link>
+                    <span>John</span>
+                    <span>Logout</span>
+                    <span className='write'>
+                        <Link classname="link" to="/write">Write</Link>
+                    </span>
+                </div>
             </div>
         </div>
     )
