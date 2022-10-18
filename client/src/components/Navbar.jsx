935f34dbfefa9ec00ce60661d6a7ad2e57e15463
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-18 20:55:03 +0800

    updated login/logout pages, added cookie-parser

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Logo from "../img/logo.PNG"
 import { Link } from "react-router-dom"
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const Navbar = () => {
+
+    const { currentUser, logout } = useContext(AuthContext)
+
     return (
         <div className='navbar'>
             <div className="container">
@@ -28,8 +33,14 @@ const Navbar = () => {
                     <Link className='link' to="/?cat=food">
                         <h6>FOOD</h6>
                     </Link>
-                    <span>John</span>
-                    <span>Logout</span>
+                    <span>{currentUser?.username}</span>
+                    {currentUser ? (
+                        <span onClick={logout}>Logout</span>
+                    ) : (
+                        <Link className='link' to="/login">
+                            Login
+                        </Link>
+                    )}
                     <span className='write'>
                         <Link className="link" to="/write">Write</Link>
                     </span>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios"
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const Login = () => {
 
@@ -14,6 +16,8 @@ const Login = () => {
 
     const navigate = useNavigate()
 
+    const { login } = useContext(AuthContext)
+
     const handleChange = e => {
         setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }))
     }
@@ -21,7 +25,7 @@ const Login = () => {
     const handleSubmit = async e => {
         e.preventDefault()
         try {
-            await axios.post("/auth/login", inputs)
+            await login(inputs)
             navigate("/")
         } catch (err) {
             setError(err.response.data)
