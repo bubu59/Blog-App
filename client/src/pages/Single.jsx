7001499a2805d800e178d97566203fba7a3e1f98
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2022-10-19 18:35:41 +0800

    update, delete post done

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -48,10 +48,15 @@ const Single = () => {
         }
     }
 
+    const getText = (html) => {
+        const doc = new DOMParser().parseFromString(html, "text/html")
+        return doc.body.textContent
+    }
+
     return (
         <div className="single">
             <div className="content">
-                <img src={post?.img} alt="" />
+                <img src={`../upload/${post?.img}`} alt="" />
                 <div className="user">
                     {post.userImg && <img
                         src={post.userImg}
@@ -62,14 +67,14 @@ const Single = () => {
                         <p>Posted {moment(post.date).fromNow()}</p>
                     </div>
                     {currentUser.username === post.username && <div className="edit">
-                        <Link to="/write?edit=2">
+                        <Link to="/write?edit=2" state={post}>
                             <img src={Edit} alt="" />
                         </Link>
                         <img onClick={handleDelete} src={Delete} alt="" />
                     </div>}
                 </div>
                 <h1>{post.title}</h1>
-                {post.desc}
+                {getText(post.desc)}
             </div>
             <Menu cat={post.cat} />
         </div>
